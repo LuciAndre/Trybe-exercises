fcd801ea49e0f15e0f6262c52001435df3023399
Andrea <andrealh08@gmail.com>
2021-07-10 23:22:51 -0300

    Algumas coisas no segundo exercicio, pode aumentar

---
+++ bloco_5/dia_1/script.js
@@ -0,0 +1,7 @@
+let titulos3 = document.querySelectorAll('h3');
+for( let index = 0; index < titulos3.length; index +=1) {
+  titulos3[index].style.color = "blue";
+}
+
+let container = document.getElementById('container');
+  container.style.border = "solid black 4px";
